c9d20a159a8ff912fff87267da444c2e5901a136
Eliuseus <141990266+Eliuseus@users.noreply.github.com>
2023-08-14 06:52:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const elements = document.querySelectorAll('a');
+for ( i= 0; i < elements.length; i++)  {   elements[i].style.color = "red";
+}
+
+function myFun(){
+         alert("Hello there!");
+}
